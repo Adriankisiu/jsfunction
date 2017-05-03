52085aa73ec688affb933784611147969a12a3d2
Adrian <tommazost@gmail.com>
2017-05-03 20:57:45 +0200

    poprawki do js

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,14 @@
-// plik scripts.js
-
 function getTriangleArea (a, h) {
-
-	if (a <= 0) {
-		alert("Nieprawidłowe dane");
-	}
-	if (h <= 0) {
-		alert("Nieprawidłowe dane");
-	}
-
 	var triangleArea = a*h/2;
+
+	if ((a <= 0) || (h <= 0))  {
+		alert("Nieprawidłowe dane");
+	}
+	
 	return triangleArea;
 }
 
-console.log(getTriangleArea(10, 6) );
-console.log(getTriangleArea(2, 4) );
-console.log(getTriangleArea(4, 6) );
-console.log(getTriangleArea(8, 10) );
+console.log(getTriangleArea(10, 6));
+console.log(getTriangleArea(2, 4));
+console.log(getTriangleArea(4, 6));
+console.log(getTriangleArea(8, 10));
